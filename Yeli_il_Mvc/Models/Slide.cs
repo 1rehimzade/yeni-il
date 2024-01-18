878aadd2b4f1811e639d6a602346dfbfced94b2c
nihatab104 <146129654+1rehimzade@users.noreply.github.com>
2024-01-18 13:09:40 +0400

    ggg

---
+++ Yeli_il_Mvc/Models/Slide.cs
@@ -0,0 +1,13 @@
+﻿using Yeli_il_Mvc.Models.Base;
+
+namespace Yeli_il_Mvc.Models;
+
+public class Slide : BaseEntity
+{
+	public string Title { get; set; } = null!;
+	public string? Description { get; set; }
+	public string ImageUrl { get; set; } = null!;
+	public string ButtonTitle { get; set; } = "Shop Now";
+	public int Order { get; set; }
+	public bool IsActive { get; set; }
+}
